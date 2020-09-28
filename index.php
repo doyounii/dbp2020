04a5f0069ad7f92cd0b5cf77eb3c65d28b17758e
leesoyoun <49148640+leesoyoun@users.noreply.github.com>
2020-09-28 20:04:26 +0900

    Add files via upload

--- index.php
+++ index.php
@@ -17,28 +17,24 @@
   $delete_link = '';
   $author = '';
 
-
   if (isset($_GET['id'])) {
       $filtered_id = mysqli_real_escape_string($link, $_GET['id']);
-      $query = "SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id
-       WHERE topic.id={$filtered_id}";
+      $query = "SELECT * FROM topic LEFT JOIN author ON topic.author_id = author.id WHERE topic.id={$filtered_id}";
       $result = mysqli_query($link, $query);
       $row = mysqli_fetch_array($result);
       $article['title'] = htmlspecialchars($row['title']);
       $article['description'] = htmlspecialchars($row['description']);
       $article['name'] = htmlspecialchars($row['name']);
 
-
       $update_link = '<a href="update.php?id='.$_GET['id'].'">update</a>';
       $delete_link = '
-        <form action="process_delete.php" method="post">
-          <input type="hidden" name="id" value="'.$_GET['id'].'">
-          <input type="submit" value="delete">
-        </form>
-      ';
+      <form action="process_delete.php" method="post">
+        <input type="hidden" name="id" value="'.$_GET['id'].'">
+        <input type="submit" value="delete">
+      </form>
+    ';
+      $author = "<p>by {$article['name']}</p>";
   }
-     $author = "<p>by {$article['name']}</p>";
-
  ?>
 
 <!DOCTYPE html>
