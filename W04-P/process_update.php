ca7af8b7905ac49754c97a19f0459a7d0dc9ff7e
leesoyoun <49148640+leesoyoun@users.noreply.github.com>
2020-10-08 10:05:38 +0900

    Update process_update.php

--- W04-P/process_update.php
+++ W04-P/process_update.php
@@ -2,20 +2,24 @@
     $link = mysqli_connect('localhost:3307', 'root', 'rootroot', 'dbp');
     settype($_POST['id'], 'integer');
     $filtered = array(
+        'title' => mysqli_real_escape_string($link, $_POST['title']),
+        'description' => mysqli_real_escape_string($link, $_POST['description']),
         'id' => mysqli_real_escape_string($link, $_POST['id'])
     );
-
     $query = "
-        DELETE
-            FROM author
-            WHERE id = {$filtered['id']}
+        UPDATE topic
+            SET
+                title = '{$filtered['title']}',
+                description = '{$filtered['description']}'
+            WHERE
+                id = '{$filtered['id']}'
     ";
 
-    $result = mysqli_query($link, $query);
+    $result = mysqli_multi_query($link, $query);
     if ($result == false) {
-        echo '삭제하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
+        echo '수정하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
         error_log(mysqli_error($link));
     } else {
-        header('Location: author.php');
+        echo '성공했습니다. <a href="index.php">돌아가기</a>';
     }
 
