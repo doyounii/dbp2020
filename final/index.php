0f868b694ed0511b3ddf4476f35a9c3575238a69
leesoyoun <49148640+leesoyoun@users.noreply.github.com>
2020-12-14 01:41:42 +0900

    Update index.php

--- final/index.php
+++ final/index.php
@@ -16,6 +16,7 @@
         text-align: center;
         background-color: #a8daf9;
         border-radius:100px;
+        border: 10px solid #ffffff;
         padding: 20px;
         margin-bottom: 60px;
       }
@@ -50,6 +51,7 @@
       a{
         color:black;
       }
+
     </style>
   </head>
   <body>
@@ -58,19 +60,40 @@
         <h1> 채용 정보 서비스 </h1>
       </div>
       <div id="left">
-        <h3><a> 지역별 채용 정보 </a></h3>
+        <h3 style="color:black"> 지역별 채용 정보 </h3>
         <form action="local.php" method="get">
-         <input type="text" name="name" placeholder="ex) 서울, 전북, 경북, 대구">
+         <input type="text" name="name" placeholder="ex) 서울, 경기, 경북, 대구">
          <input type="submit" value="Select"></br>
       </form>
       </div>
       <div id="center">
-        <h3><a> 직종별 채용 정보 </a></h3>
+        <h3><a href="saltpnm.php"> 임금형태별 채용 정보 </a></h3>
       </div>
       <div id="right">
         <h3><a href="closedt.php"> 마감일 순 채용 정보 </a></h3>
       </div>
-     
+      <div id="center">
+        <h3><a> 임금별 TOP 15 </a></h3>
+        <form action="salary.php" name="saltpnm" method="POST" >
+                      <input type = "radio" name = "saltpnm" value = "시급">시급
+                      <input type = "radio" name = "saltpnm" value = "월급">월급
+                      <input type = "radio" name = "saltpnm" value = "연봉">연봉
+                    </select>
+                    <input type="Submit" value="Select">
+                </form>
+      </div>
+      <div id="right">
+        <h3><a> 지원자격별 채용 정보  </a></h3>
+        <form action="career.php" method="post">
+                    <select name="career">
+                      <option  label ="경력 선택" hidden>
+                        <option value="경력">경력</option>
+                        <option value="신입">신입</option>
+                        <option value="관계없음">관계없음</option>
+                    </select>
+                    <input type="Submit" value="Select">
+                </form>
+      </div>
     </div>
   </body>
 </html>
